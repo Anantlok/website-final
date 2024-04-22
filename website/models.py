d3cc468ce76a58bcdbb268c54fcc15cd79aa8b16
Anantlok <167135027+Anantlok@users.noreply.github.com>
2024-04-22 14:56:10 +0530

    Update models.py

--- website/models.py
+++ website/models.py
@@ -5,11 +5,12 @@ class Note(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     data = db.Column(db.String(10000))
     date = db.Column(db.DateTime(timezone=True), default=func.now())
+    user_id = db.Column(db.Interger, db.ForeignKey('user.id'))
 
-      
 
 class User(db.Model, UserMixin):
       id = db.Column(db.Integer, primary_key=True)
       email = db.Column(db.String(150), unique=True)
       password1 = db.Column(db.String(150))
       first_name = db.Column(db.String(150))
+      notes = db.relationship('Note') #stores all the notes from the above class
